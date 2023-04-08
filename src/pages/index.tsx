be2e9443d670818adfe2c7e034d89cb38d70f89f
glacykm11 <glacy.kelly3@gmail.com>
2023-04-08 11:53:20 -0300

    adding new pictures

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 /* eslint-disable @next/next/no-img-element */
 import Navbar from "@/components/Navbar";
 import pizzaComFundo from "../assets/pizza-com-fundo.png";
+import pizzariaComFundo from "../assets/pizzaria-com-fundo.png";
 import styled from "styled-components";
 import Image from "next/image";
 import Button from "@/components/Button";
@@ -14,15 +15,21 @@ export default function Home() {
           <Image src={pizzaComFundo} alt="pizza" width={1000} />
         </FirstPage>
         <SecondPage>
-          <Text>
-            Desde 1980 produzindo as melhores pizzas da região. Nosso rodízio
-            oferece mais de 60 sabores entre pizzas, massas e porções.
-          </Text>
-          <Text>
-            Você pode escolher se prefere o serviço à la carte, ou encomendar
-            nossas deliciosas pizzas pelo nosso delivery.
-          </Text>
-          <Button color={"secondary"} />
+          <Image src={pizzariaComFundo} alt="pizza" width={500} />
+          <TextWrapper>
+            <Title>
+              <Underscore>Nossa Pizzaria</Underscore>
+            </Title>
+            <Text>
+              Desde 1980 produzindo as melhores pizzas da região. Nosso rodízio
+              oferece mais de 60 sabores entre pizzas, massas e porções.
+            </Text>
+            <Text>
+              Você pode escolher se prefere o serviço à la carte, ou encomendar
+              nossas deliciosas pizzas pelo nosso delivery.
+            </Text>
+            <Button color={"secondary"} />
+          </TextWrapper>
         </SecondPage>
       </Main>
     </>
@@ -43,9 +50,22 @@ const SecondPage = styled.div`
   height: 45rem;
   padding: 0 34px;
   text-align: center;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
+
+const TextWrapper = styled.div`
+  width: 40%;
+`;
+
+const Title = styled.h2``;
+
+const Underscore = styled.u``;
 
 const Text = styled.p`
   color: var(--terciary-color);
   font-size: 15px;
+  text-align: justify;
+  margin: 16px 0;
 `;
