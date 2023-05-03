e88942fe158067675c56457f669e17270b2d5206
Glacy <glacykelly@ciandt.com>
2023-05-03 15:03:19 -0300

    reorganizando estilos da página inicial

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,50 +1,42 @@
 /* eslint-disable @next/next/no-img-element */
 import Navbar from "@/components/Navbar";
-import pizzaComFundo from "../assets/pizza-com-fundo.png";
-import pizzariaComFundo from "../assets/pizzaria-com-fundo.png";
+// import pizzaComFundo from "../assets/pizza-com-fundo.png";
+// import pizzariaComFundo from "../assets/pizzaria-com-fundo.png";
 import styled from "styled-components";
 import Image from "next/image";
 import Button from "@/components/Button";
-import fundoPizza2 from "../assets/background-second-page.png";
+// import fundoPizza2 from "../assets/background-second-page.png";
 import Modal from "@/components/Modal";
 import React from "react";
 
 export default function Home() {
   const [openModal, setOpenModal] = React.useState(false);
-  
+
   return (
     <>
+      <div>
+        <Navbar onClick={() => setOpenModal(true)} />
+      </div>
       <FirstPage>
-        <Navbar onClick={() => setOpenModal(true)}/>
-        <Image src={pizzaComFundo} alt="pizza" style={{ objectFit: 'contain', minWidth: '500px', minHeight: '400px' }} />
+        <PizzaImage src="/assets/images/pizza-com-fundo.png" />
       </FirstPage>
       <SecondPage>
-        <Image
-          src={fundoPizza2}
-          alt="pizza"
-          style={{ backgroundImage: `url(${fundoPizza2})`, width: '100%', position: 'relative' }}
-        />
-        <ContentWrapper>
-          <Image
-            src={pizzariaComFundo}
-            alt="pizza"
-            width={500}
-            style={{ backgroundImage: `url(${pizzariaComFundo})` }}
-          />
-          <TextWrapper>
-            <Title>Nossa Pizzaria</Title>
-            <Text>
-              Desde 1980 produzindo as melhores pizzas da região. Nosso rodízio
-              oferece mais de 60 sabores entre pizzas, massas e porções.
-            </Text>
-            <Text>
-              Você pode escolher se prefere o serviço à la carte, ou encomendar
-              nossas deliciosas pizzas pelo nosso delivery.
-            </Text>
-            <Button color={"secondary"} onClick={() => setOpenModal(true)}/>
-            <Modal open={openModal} onClick={() => setOpenModal(false)}/>
-          </TextWrapper>
-        </ContentWrapper>
+        <div>
+          <PizzasImages src="/assets/images/pizzaria-com-fundo.png" />
+        </div>
+        <TextWrapper>
+          <Title>Nossa Pizzaria</Title>
+          <Text>
+            Desde 1980 produzindo as melhores pizzas da região. Nosso rodízio
+            oferece mais de 60 sabores entre pizzas, massas e porções.
+          </Text>
+          <Text>
+            Você pode escolher se prefere o serviço à la carte, ou encomendar
+            nossas deliciosas pizzas pelo nosso delivery.
+          </Text>
+          <Button color={"secondary"} onClick={() => setOpenModal(true)} />
+          <Modal open={openModal} onClick={() => setOpenModal(false)} />
+        </TextWrapper>
       </SecondPage>
     </>
   );
@@ -52,27 +44,34 @@ export default function Home() {
 
 const FirstPage = styled.div`
   background-color: var(--primary-color);
-  padding: 0 34px;
+  padding: 0 32px;
   text-align: center;
 `;
 
+const PizzaImage = styled.img`
+  object-fit: contain;
+  width: 80%;
+`
+
 const SecondPage = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  align-items: center;
+  justify-content: center;
+  gap: 8rem;
   background-color: var(--secondary-color);
+  padding: 32px;
+  height: 60rem;
 `;
 
-const ContentWrapper = styled.div`
-  display: grid;
-  grid-template-columns: 34rem 28rem;
-  grid-template-rows: 1fr;
-  position: absolute;
-  top: 63rem;
-  left: 20rem;
-`;
+const PizzasImages = styled.img`
+  width: 100%;
+  height: 22rem;
+  object-fit: contain;
+`
 
 const TextWrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
+  width: 32rem;
 `;
 
 const Title = styled.h2`
