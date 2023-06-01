c6c2de81d585f93c9aa6c31808960e5d51c9be5d
Glacy Gomes <glacy.kelly3@gmail.com>
2023-05-31 22:30:30 -0300

    Adicionando mais dados mockados

--- src/utils/constants/index.ts
+++ src/utils/constants/index.ts
@@ -1,5 +1,10 @@
 export const PIZZAS = [
     { sabor: 'Calabresa', id: 1, fatias: '8', url: '/assets/images/pizza-calabresa.png' },
     { sabor: 'Mussarela', id: 2, fatias: '8', url: '/assets/images/pizza-calabresa.png' },
-    { sabor: 'Portuguesa', id: 3, fatias: '6', url: '/assets/images/pizza-calabresa.png' },
+    { sabor: 'Calabresa', id: 3, fatias: '8', url: '/assets/images/pizza-calabresa.png' },
+    { sabor: 'Mussarela', id: 4, fatias: '8', url: '/assets/images/pizza-calabresa.png' },
+    { sabor: 'Calabresa', id: 5, fatias: '8', url: '/assets/images/pizza-calabresa.png' },
+    { sabor: 'Mussarela', id: 6, fatias: '8', url: '/assets/images/pizza-calabresa.png' },
+    { sabor: 'Calabresa', id: 7, fatias: '8', url: '/assets/images/pizza-calabresa.png' },
+    { sabor: 'Mussarela', id: 8, fatias: '8', url: '/assets/images/pizza-calabresa.png' },
 ];
